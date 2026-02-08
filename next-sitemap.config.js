0d263e77dad1e75812c86be2ad83b246e43fb86c
Kesavaram S <kesav@Mac.communityfibre.co.uk>
2026-02-08 14:34:34 +0000

    Configure auto-generated sitemap with next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://automaitee.com',
+    generateRobotsTxt: true,
+    outDir: 'public', // Ensures sitemap and robots.txt are output to public/
+};
